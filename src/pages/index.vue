dba816bad4798249a1a4f2aee14ea4ab5fa108b6
Peter <peterkimzz69@gmail.com>
2019-10-08 18:32:56 +0900

    views/ -> pages/

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img 
+      alt="Vue logo" 
+      :src="require('src/assets/imgs/logo.png')">
+    <h1>Vuejs Boilerplate</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  background-color: $oc-gray-1;
+  padding: 30px;
+}
+</style>
